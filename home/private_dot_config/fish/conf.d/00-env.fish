05b6339d9307763bd8eb53102406052a528cb1f4
Red Buttons <knopki@duck.com>
2025-07-31 19:55:10 +0300

    Update home/.config/fish/conf.d/00-env.fish

--- home/private_dot_config/fish/conf.d/00-env.fish
+++ home/private_dot_config/fish/conf.d/00-env.fish
@@ -1,3 +1,5 @@
+fish_add_path --prepend "$HOME/.local/bin"
+
 set -q XDG_CACHE_HOME || set -x XDG_CACHE_HOME "$HOME/.cache"
 set -q XDG_CONFIG_HOME || set -x XDG_CONFIG_HOME "$HOME/.config"
 set -q XDG_DATA_HOME || set -x XDG_DATA_HOME "$HOME/.local/share"
