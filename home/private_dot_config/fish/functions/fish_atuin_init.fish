72127b75ec85c6765fc069c2200f99247e337a95
Red Buttons <knopki@duck.com>
2025-07-31 13:33:24 +0300

    Add home/.bash_profile Update home/.bashrc Add home/.config/fish/conf.d/00-env.fish Update home/.config/fish/functions/fish_atuin_init.fish Update home/.config/fish/functions/fish_starship_init.fish

--- home/private_dot_config/fish/functions/fish_atuin_init.fish
+++ home/private_dot_config/fish/functions/fish_atuin_init.fish
@@ -2,6 +2,5 @@ function fish_atuin_init
     status is-interactive || return 10
     type -q atuin || return 11
     test -e "$XDG_CONFIG_HOME/atuin/config.toml" || return 12
-    ! set -q ATUIN_SESSION || return 13
     atuin init fish --disable-up-arrow | source
 end
