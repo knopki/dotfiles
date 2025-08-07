e42215f623559bae9701b91aa6a75de9cd78c346
Red Buttons <knopki@duck.com>
2025-08-07 17:14:52 +0300

    Remove home/.config/fish/functions/fish_atuin_init.fish Remove home/.config/fish/functions/fish_fzf_init.fish Remove home/.config/fish/functions/fish_starship_init.fish

--- home/private_dot_config/fish/functions/fish_starship_init.fish
+++
@@ -1,7 +0,0 @@
-function fish_starship_init
-    status is-interactive || return 10
-    type -q starship || return 11
-    test -e "$XDG_CONFIG_HOME/starship.toml" || return 12
-    test "$TERM" = dumb || return 14
-    starship init fish | source
-end
