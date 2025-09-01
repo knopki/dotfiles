fc534b01e09d35df1d6340358fdcb6b5cb921a49
Red Buttons <knopki@duck.com>
2025-09-01 12:55:21 +0300

    Update home/.config/fish/config.fish

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 set -g fish_greeting ""
-set -x fish_history "$(hostname -s)"
+set -x fish_history "$(hostname -s | tr - _)"
 set -g fish_key_bindings fish_hybrid_key_bindings
