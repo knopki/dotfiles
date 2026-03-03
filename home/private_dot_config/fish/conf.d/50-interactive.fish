f0e8229040b6b3d73ee4f313177da51402ba7e6f
Red Buttons <knopki@duck.com>
2026-03-03 19:09:24 +0300

    fix(fish): starship startup

--- home/private_dot_config/fish/conf.d/50-interactive.fish
+++ home/private_dot_config/fish/conf.d/50-interactive.fish
@@ -26,7 +26,7 @@ function _starship_init
     status is-interactive || return 10
     type -q starship || return 11
     test -e "$XDG_CONFIG_HOME/starship.toml" || return 12
-    test "$TERM" = dumb || return 14
+    test "$TERM" = dumb && return 14
     starship init fish | source
 end
 
